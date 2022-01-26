491c3ed7889c0692f8194b617d7c6165f62b769c
Cardiff MRC Pathdfinder <35573120+CardiffMRCPathfinder@users.noreply.github.com>
2022-01-26 11:58:39 +0000

    Update Example.command.line.sh

--- Example.command.line.sh
+++ Example.command.line.sh
@@ -1,2 +1,2 @@
 ### Assuming all files are in the same directory
-Rscript summaRygwasqc.V1.3.3.R --GWAS ckqny.scz2snpres.gz --HRC HRC.Chr --PRS F --mthres 0.01 --ithres 0.8 --MHC T
+Rscript summaRygwasqc.V1.3.4.R --GWAS ckqny.scz2snpres.gz --HRC HRC.Chr --PRS F --mthres 0.01 --ithres 0.8 --MHC T
